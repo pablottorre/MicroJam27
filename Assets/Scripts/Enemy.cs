b8f9f6c91ee0ca4bfc025db47a417d448e476989
pablo Torre <pablo.ttorre@gmail.com>
2024-11-15 21:42:22 -0300

    Enemy class parent and moving left to right

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int life;
+    [SerializeField] protected float speed;
+    [SerializeField] protected int colorNumber;
+    [SerializeField] protected GameObject door;
+    [SerializeField] protected GameObject nexus;
+
+    protected void Update()
+    {
+        if (door.gameObject.activeInHierarchy)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(door.transform.position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(nexus.transform.position.x,transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+    }
+
+
+}
