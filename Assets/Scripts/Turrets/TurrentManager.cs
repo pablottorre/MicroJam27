a449a1a2994a9fb5cc30e89a520271330610e36b
pablo Torre <pablo.ttorre@gmail.com>
2024-11-17 15:22:16 -0300

    TurretMovement

--- Assets/Scripts/Turrets/TurrentManager.cs
+++ Assets/Scripts/Turrets/TurrentManager.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 
 public class TurrentManager : MonoBehaviour
 {
-    [SerializeField] private List<Turret> turretsBlue = new List<Turret>();
-    [SerializeField] private List<Turret> turretsYellow = new List<Turret>();
-    [SerializeField] private List<Turret> turretsRed = new List<Turret>();
+    [SerializeField] private Turret turretsBlue;
+    [SerializeField] private Turret turretsYellow;
+    [SerializeField] private Turret turretsRed;
 
     //Primary
     public readonly Color BlueColor = new Color32(0, 0, 255, 255);
     public readonly Color RedColor = new Color32(255, 0, 0, 255);
-    public readonly Color YellowColor = new Color32(255, 255, 0, 255);  
-    
+    public readonly Color YellowColor = new Color32(255, 255, 0, 255);
+
     //Secondary
     public readonly Color GreenColor = new Color32(0, 128, 0, 255);
     public readonly Color OrangeColor = new Color32(255, 165, 0, 255);
     public readonly Color VioletColor = new Color32(127, 0, 255, 255);
-    
 
-    [ContextMenu("bla")]
-    public void TestFunc()
-    {
+    [SerializeField] private float minZ, maxZ;
 
-    }
+    [SerializeField] private List<GameObject> selectedTurret = new List<GameObject>();
+    [SerializeField] private float turretSpeed;
+    private int numberTurret = 0;
+
+
 
     private void Update()
     {
@@ -32,71 +33,66 @@ public class TurrentManager : MonoBehaviour
         {
             ShootBlueTurrets();
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             ShootYellowTurrets();
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             ShootRedTurrets();
         }
+
+        #region InputsTurrets
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (selectedTurret[numberTurret].transform.position.z < maxZ)
+            {
+                selectedTurret[numberTurret].transform.position += new Vector3(0, 0, turretSpeed * Time.deltaTime);
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (selectedTurret[numberTurret].transform.position.z > minZ)
+            {
+                selectedTurret[numberTurret].transform.position -= new Vector3(0, 0, turretSpeed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            numberTurret++;
+            if (numberTurret >= selectedTurret.Count)
+                numberTurret = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            numberTurret--;
+            if (numberTurret < 0)
+                numberTurret = (selectedTurret.Count - 1);
+        }
+
+        #endregion
     }
 
     #region Turrets
-
-    public void AddBlueTurret(Turret newTurret)
-    {
-        turretsBlue.Add(newTurret);
-    }
-
-    public void RemoveBlueTurret(Turret newTurret)
-    {
-        turretsBlue.Remove(newTurret);
-    }
     public void ShootBlueTurrets()
     {
-        for (int i = 0; i < turretsBlue.Count; i++)
-        {
-            turretsBlue[i].Shoot();          
-        }
+        turretsBlue.Shoot();
     }
 
-
-    public void AddYellowTurret(Turret newTurret)
-    {
-        turretsYellow.Add(newTurret);
-    }
-
-    public void RemoveYellowTurret(Turret newTurret)
-    {
-        turretsYellow.Remove(newTurret);
-    }
     public void ShootYellowTurrets()
     {
-        for (int i = 0; i < turretsYellow.Count; i++)
-        {
-            turretsYellow[i].Shoot();
-        }
+        turretsYellow.Shoot();
+
     }
 
-
-    public void AddRedTurret(Turret newTurret)
-    {
-        turretsRed.Add(newTurret);
-    }
-
-    public void RemoveRedTurret(Turret newTurret)
-    {
-        turretsRed.Remove(newTurret);
-    }
     public void ShootRedTurrets()
     {
-        for (int i = 0; i < turretsRed.Count; i++)
-        {
-            turretsRed[i].Shoot();
-        }
+        turretsRed.Shoot();
     }
 
     #endregion
