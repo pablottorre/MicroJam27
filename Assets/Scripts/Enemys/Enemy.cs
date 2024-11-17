bbfc2e95e3fa5b3e11893dbb0ea786a7b99d6f2b
Ignacio Martinez <agustinmartinezbarrios2@gmail.com>
2024-11-17 17:55:26 -0300

    Joke

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -28,8 +28,6 @@ public class Enemy : MonoBehaviour, IPoolObject<Enemy>
     {
         if (door.gameObject.activeInHierarchy)
         {
-            transform.forward = doorPosition - transform.position;
-            
             if (Vector3.Distance(transform.position, doorPosition) < 2f)
             {
                 //animacion para dmg
@@ -40,8 +38,6 @@ public class Enemy : MonoBehaviour, IPoolObject<Enemy>
         }
         else
         {
-            transform.forward = nexusPosition - transform.position;
-            
             if (Vector3.Distance(transform.position, nexusPosition) < 2f)
             {
                 nexus.TakeDamage(1);
